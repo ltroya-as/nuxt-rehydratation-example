f0304f7a6f53fba2035028958eae0d53e0e1fea1
ltroya-as <luis.troya@allansolutions.com>
2021-07-15 11:46:06 -0500

    Add example with not generated content

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,35 @@
 <template>
-  <Tutorial/>
+  <div class="p-10">
+    <button class="mb-10 px-4 py-2 bg-green-500" @click="login">Log in</button>
+
+    <div v-if="authenticated === false" class="text-2xl">
+      No authenticated content
+    </div>
+
+    <div v-if="authenticated" class="text-2xl">Authenticated content</div>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      authenticated: null,
+    }
+  },
+
+  mounted() {
+    this.authenticated = !!this.$cookies.get('LOGIN_COOKIE')
+  },
+
+  methods: {
+    login() {
+      const duration = 60 * 60 * 24 * 365
+      this.$cookies.set('LOGIN_COOKIE', 'COOKIE_CONTENT', {
+        maxAge: duration,
+        path: '/',
+      })
+    },
+  },
+}
 </script>
