29e19ba3f4ed37a99ef5852efc38157eec218d0a
ltroya-as <luis.troya@allansolutions.com>
2021-07-15 14:30:20 -0500

    Add possible solution

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,9 @@
     <p>Please click log in and reload the page to see the problem</p>
     <button class="mb-10 px-4 py-2 bg-green-500" @click="login">Log in</button>
 
-    <div v-if="!authenticated" class="text-2xl">No authenticated content</div>
+    <div v-if="authenticated === false" class="text-2xl">
+      No authenticated content
+    </div>
 
     <div v-if="authenticated" class="text-2xl">Authenticated content</div>
   </div>
@@ -13,7 +15,7 @@
 export default {
   data() {
     return {
-      authenticated: false,
+      authenticated: null,
     }
   },
 
